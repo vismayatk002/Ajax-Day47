83f3c67f14051b688753b7edd496e380aff406fe
vismayatk002 <vismayatk002@gmail.com>
2021-09-22 03:33:01 +0530

    Refactor html file to test the REST services with JSON server using Promise

--- EmployeeAjax.js
+++ EmployeeAjax.js
@@ -1,4 +1,4 @@
-let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 function makePromiseCall(methodType, url, async = true, data = null){
     return new Promise(function (resolve, reject){
         let xhr = new XMLHttpRequest();
@@ -7,7 +7,7 @@ function makePromiseCall(methodType, url, async = true, data = null){
             if(xhr.status.toString().match('^[2][0,9]{2}$')){
                 resolve(xhr.responseText);
             }
-            else if(xhr.statustoString().match('^[4,5][0,9]{2}$')){
+            else if(xhr.status.toString().match('^[4,5][0,9]{2}$')){
                 reject({
                     status: xhr.status,
                     statusText: xhr.statusText
@@ -27,27 +27,3 @@ function makePromiseCall(methodType, url, async = true, data = null){
         console.log(methodType + "request send to the server");
     });
 }
-
-const getURL = "http://localhost:3000/employees/1";
-makePromiseCall("GET", getURL, true)
-    .then(responseText => {
-        console.log("Get User Data : " + responseText);
-    })
-    .catch(error => console.log("GET Error Status : " + JSON.stringify(error)));
-
-
-const deleteURL = "http://localhost:3000/employees/3";
-makePromiseCall("DELETE", deleteURL, false)
-    .then(responseText => {
-        console.log("User Deleted : " + responseText);
-    })
-    .catch(error => console.log("DELETE Error Status : " + JSON.stringify(error)));
-
-
-const postURL = "http://localhost:3000/employees";
-const empData = {"name": "Vyshak","salary": "30000"};
-makePromiseCall("POST", postURL, true, empData)
-    .then(responseText => {
-        console.log("User Added : " + responseText);
-    })
-    .catch(error => console.log("POST Error Status : " + JSON.stringify(error)));
